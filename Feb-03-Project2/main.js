1323ba4e4bb21743a5ec94bb10e7e19024c16e00
VtntGaming <82591379+VtntGaming@users.noreply.github.com>
2026-02-03 11:19:06 +0700

    03/02 #2 JS update
    -> Make full delete into delete without "delete"
    -> Show deleted items

--- Feb-03-Project2/main.js
+++ Feb-03-Project2/main.js
@@ -5,12 +5,13 @@ async function getData() {
         let body = document.getElementById('table_body');
         body.innerHTML = '';
         for (const post of posts) {
-            body.innerHTML += `<tr>
+            let style = post.isDeleted ? 'text-decoration: line-through; color: gray;' : '';
+            let deleteBtn = post.isDeleted ? '' : `<input type='submit' value='Delete' onclick='Delete(${post.id})'>`;
+            body.innerHTML += `<tr style="${style}">
                 <td>${post.id}</td>
                 <td>${post.title}</td>
                 <td>${post.views}</td>
-                <td><input type='submit' value='Delete' onclick='
-                Delete(${post.id})'></td>
+                <td>${deleteBtn}</td>
             </tr>`
         }
     } catch (error) {
@@ -21,46 +22,76 @@ async function Save() {
     let id = document.getElementById('txt_id').value;
     let title = document.getElementById('txt_title').value;
     let views = document.getElementById('txt_views').value;
-    let getItem = await fetch('http://localhost:3000/posts/' + id);
-    if (getItem.ok) {
-        //edit
-        let res = await fetch('http://localhost:3000/posts/'+id, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title: title,
-                views: views
+    
+    if (id) {
+        // Edit existing post
+        let getItem = await fetch('http://localhost:3000/posts/' + id);
+        if (getItem.ok) {
+            let res = await fetch('http://localhost:3000/posts/' + id, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    title: title,
+                    views: views
+                })
             })
-        })
-        if (res.ok) {
-            console.log("thanh cong");
+            if (res.ok) {
+                console.log("cap nhat thanh cong");
+                getData();
+            }
         }
     } else {
-        //create
+        // Create new post with auto-increment ID
+        let allPosts = await fetch('http://localhost:3000/posts');
+        let posts = await allPosts.json();
+        let maxId = 0;
+        for (const post of posts) {
+            let postId = parseInt(post.id);
+            if (postId > maxId) {
+                maxId = postId;
+            }
+        }
+        let newId = (maxId + 1).toString();
+        
         let res = await fetch('http://localhost:3000/posts', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                id: id,
+                id: newId,
                 title: title,
-                views: views
+                views: views,
+                isDeleted: false
             })
         })
         if (res.ok) {
-            console.log("thanh cong");
+            console.log("tao moi thanh cong");
+            getData();
         }
     }
 }
 async function Delete(id) {
-    let res = await fetch('http://localhost:3000/posts/' + id, {
-        method: 'delete'
-    })
-    if (res.ok) {
-        console.log("xoa thanh cong");
+    // Soft delete: update isDeleted to true instead of actually deleting
+    let getItem = await fetch('http://localhost:3000/posts/' + id);
+    if (getItem.ok) {
+        let post = await getItem.json();
+        let res = await fetch('http://localhost:3000/posts/' + id, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                ...post,
+                isDeleted: true
+            })
+        })
+        if (res.ok) {
+            console.log("xoa mem thanh cong");
+            getData();
+        }
     }
 }
 getData();
